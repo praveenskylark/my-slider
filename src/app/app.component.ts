6faa43118683b448fb06cb459e9bdf2ea50550c8
praveenskylark <skylarkpraveen@gmail.com>
2022-02-10 10:23:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { convertFromMaybeForwardRefExpression } from '@angular/compiler/src/render3/util';
 import { Component } from '@angular/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-slider';
+  images = ["../assets/coffee.jpg","../assets/coffee1.jpg","../assets/coffee2.jpg"];
+  public isMenuCollapsed = true;
 }
